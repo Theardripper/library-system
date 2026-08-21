d6b2a0ff1a64c2135a354a1d32b52478d28f8f4c
Theardripper <eduardomoura512@outlook.com>
2026-08-20 23:48:30 -0300

    feat: the auth controller was initialized

---
+++ api/api/src/main/java/com/library/api/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.library.api.controller;
+
+import org.springframework.security.authentication.AuthenticationManager;
+
+public class AuthController {
+
+    private final AuthenticationManager authManager;
+
+}
