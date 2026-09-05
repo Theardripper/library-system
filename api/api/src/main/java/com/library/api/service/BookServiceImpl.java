bd1cb3f317793817c24ce873cc94da2b400ebf28
Theardripper <eduardomoura512@outlook.com>
2026-09-04 23:27:29 -0300

    method create on BookServiceImpl was finished

--- api/api/src/main/java/com/library/api/service/BookServiceImpl.java
+++ api/api/src/main/java/com/library/api/service/BookServiceImpl.java
@@ -4,8 +4,10 @@ import com.library.api.dto.Response.BookResponseDTO;
 import com.library.api.model.Book;
 import com.library.api.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class BookServiceImpl {
+@Service
+public class BookServiceImpl implements BookService{
 
     private final BookRepository bookRepository;
 
@@ -14,9 +16,17 @@ public class BookServiceImpl {
         this.bookRepository = bookRepository;
     }
 
+    @Override
     public BookResponseDTO create(BookResponseDTO dto){
         Book book = new Book();
         book.setName(dto.getName());
+        book.setAuthor(dto.getAuthor());
+        book.setYear(dto.getYear());
+        book.setSynopis(dto.getSynopis());
+        book.setEdition(dto.getEdition());
+
+        Book salved = bookRepository.save(book);
+        return toResponseDTO(salved);
 
     }
 
