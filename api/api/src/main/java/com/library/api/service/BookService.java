2545296fc646f526b03a36ae112493313c9ff0f3
Theardripper <eduardomoura512@outlook.com>
2026-09-16 22:11:38 -0300

    feat: BookServiceImpl was finished

--- api/api/src/main/java/com/library/api/service/BookService.java
+++ api/api/src/main/java/com/library/api/service/BookService.java
@@ -1,13 +1,14 @@
 package com.library.api.service;
 
+import com.library.api.dto.Request.BookRequestDTO;
 import com.library.api.dto.Response.BookResponseDTO;
 
 import java.util.List;
 
 public interface BookService {
-    BookResponseDTO create(BookResponseDTO dto);
+    BookResponseDTO create(BookRequestDTO dto);
     BookResponseDTO findById(Long id);
     List<BookResponseDTO> findAll();
-    BookResponseDTO update(Long id, BookResponseDTO dto);
+    BookResponseDTO update(Long id, BookRequestDTO dto);
     void delete(Long id);
 }
--- api/api/src/main/java/com/library/api/service/BookServiceImpl.java
+++ api/api/src/main/java/com/library/api/service/BookServiceImpl.java
@@ -1,10 +1,10 @@
 package com.library.api.service;
 
+import com.library.api.dto.Request.BookRequestDTO;
 import com.library.api.dto.Response.BookResponseDTO;
 import com.library.api.model.Book;
 import com.library.api.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,7 +21,7 @@ public class BookServiceImpl implements BookService{
     }
 
     @Override
-    public BookResponseDTO create(BookResponseDTO dto){
+    public BookResponseDTO create(BookRequestDTO dto){
         Book book = new Book();
         book.setName(dto.getName());
         book.setAuthor(dto.getAuthor());
@@ -36,15 +36,38 @@ public class BookServiceImpl implements BookService{
     @Override
     public BookResponseDTO findById(Long id){
         Book book = bookRepository.findById(id)
-                .orElseThrow(() -> new ConfigDataResourceNotFoundException("Livro não encontrado com id"));
+                .orElseThrow(() -> new ResourceNotFoundException("Livro não encontrado com id " + id));
         return toResponseDTO(book);
     }
 
-    public List<BookResponseDTO> listAll(){
+    @Override
+    public List<BookResponseDTO> findAll(){
         return bookRepository.findAll()
                 .stream()
                 .map(this::toResponseDTO)
-                .collect(Collectors.toUnmodifiableList());
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public BookResponseDTO update(Long id, BookRequestDTO dto){
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Livro não encontrado com id " + id));
+
+        book.setName(dto.getName());
+        book.setAuthor(dto.getAuthor());
+        book.setYear(dto.getYear());
+        book.setSynopis(dto.getSynopis());
+        book.setSynopis(dto.getSynopis());
+
+        Book updated = bookRepository.save(book);
+        return toResponseDTO(updated);
+    }
+
+    public void delete(Long id){
+        if (!bookRepository.existsById(id)){
+            throw new ResourceNotFoundException("Livro não encontrado com id " + id);
+        }
+        bookRepository.deleteById(id);
     }
 
     private BookResponseDTO toResponseDTO(Book book){
